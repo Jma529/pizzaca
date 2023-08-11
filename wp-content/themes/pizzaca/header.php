5f83027917bbe8009910793077a7cdf468acf2dd
Jill Augustus <jma529@gmail.com>
2023-08-11 12:47:55 +1000

    Woocommerce updates

---
+++ wp-content/themes/pizzaca/header.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Pizzaca
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'pizzaca' ); ?></a>
+
+	<header id="masthead" class="header">
+		<div class="wrapper header-wrapper">
+		<div class="site-branding">
+			<div class="header-logo">
+				<a href="<?php echo get_home_url(); ?>">
+					<img class="logo" src="<?php echo get_template_directory_uri(); ?>/media/images/pizzaca-logo-light.svg" alt="<?php bloginfo( 'name' ); ?>" />
+				</a>
+			</div>
+		</div>
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Mobile Menu', 'pizzaca' ); ?></button>
+			<div class="menu-items">
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'header-menu',
+				)
+			);
+			?>
+			</div>
+			<div class="cart-icon">
+		<a class="basketicon" href="<?php echo wc_get_checkout_url(); ?>" title="<?php _e( 'View your shopping cart' ); ?>"> <i class="fa fa-shopping-cart"></i> <?php echo WC()->cart->get_cart_total(); ?></a>
+		</div>
+		</nav><!-- #site-navigation -->
+		
+		</div>
+	</header><!-- #masthead -->
---
+++ wp-content/themes/pizzaca/woocommerce/archive-product.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.4.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+
+$table_1 = '[product_table category="221, 267" product_table columns="name: Starters and Salads, short-description: Descripton, price, add-to-cart"]';
+$table_2 = '[product_table category="219" product_table columns="name: Pizza Type, short-description: Description, price, quick-view: Choose add-ons"]';
+$table_3 = '[product_table category="268" product_table columns="name: Pastas, short-description: Descripton, price, add-to-cart" ]';
+$table_4 = '[product_table category="270" product_table columns="name: Mains, short-description: Descripton, price, add-to-cart" ]';
+$table_5 = '[product_table category="272" product_table columns="name: Kids, price, add-to-cart" ]';
+$table_6 = '[product_table category="279" product_table columns="name: Desserts, price, add-to-cart" ]';
+
+
+/**
+ * Hook: woocommerce_before_main_content.
+ *
+ * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+ * @hooked woocommerce_breadcrumb - 20
+ * @hooked WC_Structured_Data::generate_website_data() - 30
+ */
+// do_action( 'woocommerce_before_main_content' );
+
+?>
+<main id="primary" class="site-main">
+  <section class="top">
+    <div class="wrapper">
+    <h1 class="title">	<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
+		<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+	<?php endif; ?></h1>
+<!-- wp:paragraph {"align":"left"} -->
+<p>Deliveries and take-away hours: Tues-Sun, 4:30pm-9pm<br><strong>Deliveries: Start at 5pm, Please allow 45 minutes
+<br>Local pick-up: You will receive a text within 15 minutes with your pick-up time.</strong><br>Minimum order: $25<br>Delivery fee: $5</p>
+
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link" href="http://localhost:8888/pizzaca/cart/">View cart</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
+<h2>Starters &amp; Salads</h2>
+<p> <?php echo do_shortcode($table_1); ?> </p>
+<h2>Wood Fired Pizzas</h2>
+<p> <?php echo do_shortcode($table_2); ?> </p>
+<h2>Pastas</h2>
+<p> <?php echo do_shortcode($table_3); ?> </p>
+<h2>Mains</h2>
+<p> <?php echo do_shortcode($table_4); ?> </p>
+<h2>Kids</h2>
+<p> <?php echo do_shortcode($table_5); ?> </p>
+<h2>Desserts</h2>
+<p> <?php echo do_shortcode($table_6); ?> </p>
+
+</div>
+</section>
+</main>
+<?php
+/**
+ * Hook: woocommerce_sidebar.
+ *
+ * @hooked woocommerce_get_sidebar - 10
+ */
+// do_action( 'woocommerce_sidebar' );
+
+get_footer();
+?>
